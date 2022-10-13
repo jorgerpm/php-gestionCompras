161ef6e8c498382f1537181a2b39a1bdfa134d14
jorgerpm <jorgepm.4382@gmail.com>
2022-10-13 15:23:53 -0500

    cambio en la pantalla home

--- Views/home.php
+++ Views/home.php
@@ -1,18 +1,17 @@
 <main class="app-content">
     <div class="app-title" style="height: 50px">
         <div>
-            <h1><i class="fa fa-dashboard"></i> Blank Page</h1>
-            <p>Start a beautiful journey here</p>
+            <span class="tamañoTitulo"><i class="fa fa-dashboard"></i> P&aacute;gina principal</span>
         </div>
         <ul class="app-breadcrumb breadcrumb">
             <li class="breadcrumb-item"><i class="fa fa-home fa-lg"></i></li>
-            <li class="breadcrumb-item"><a href="#">Blank Page</a></li>
+            <li class="breadcrumb-item"><a href="#">P&aacute;gina principal</a></li>
         </ul>
     </div>
     <div class="row">
         <div class="col-md-12">
             <div class="tile">
-                <div class="tile-body">Create a beautiful dashboard</div>
+                <div class="tile-body"></div>
             </div>
         </div>
     </div>
